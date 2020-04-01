2dc27588cae3b141a4c43e1cc07a48547f4ba657
tairhadad <tairhadad1@gmail.com>
2020-04-01 11:36:43 +0300

    Welcome page

--- app/src/main/java/com/example/bsport/HomeActivity.java
+++ app/src/main/java/com/example/bsport/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.example.bsport;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Toolbar mToolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mToolBar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setTitle("BSport");
     }
 }
