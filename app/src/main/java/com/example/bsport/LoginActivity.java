4d2a527f757fc0bdd708fc0534fb8a9b7a027028
tairhadad <tairhadad1@gmail.com>
2020-04-05 14:50:46 +0300

    Register page + Design new

--- app/src/main/java/com/example/bsport/LoginActivity.java
+++ app/src/main/java/com/example/bsport/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__login);
-    }
+    } //
     @Override
     protected void onStart() {
         super.onStart();
