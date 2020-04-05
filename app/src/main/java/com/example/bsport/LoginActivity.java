b9acfa5eb95670d91990920f0b1cd189a44aaea9
yoni6232@gmail.com <yoni>
2020-04-05 14:41:21 +0300

    Login

--- app/src/main/java/com/example/bsport/LoginActivity.java
+++ app/src/main/java/com/example/bsport/LoginActivity.java
@@ -4,17 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEmail,UserPassword;
+    private TextView NeedNewAccountLink ,ForgetPasswordLink;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__login);
+
+        initilizedFields();
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToLRegisterActivity();
+            }
+        });
     }
+
+    private void initilizedFields() {
+        LoginButton = (Button) findViewById(R.id.login_button);
+        LoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = ( EditText) findViewById(R.id.login_email);
+        UserPassword = ( EditText) findViewById(R.id.login_password);
+        ForgetPasswordLink = ( TextView) findViewById(R.id.forget_password_link);
+        NeedNewAccountLink = ( TextView) findViewById(R.id.Sign_Up_link);
+
+
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -28,5 +55,10 @@ public class LoginActivity extends AppCompatActivity {
         Intent MainIntent = new Intent(  LoginActivity.this,HomeActivity.class );
         startActivity(MainIntent);
     }
+    private void SendUserToLRegisterActivity() {
+        Intent RegisterIntent = new Intent(  LoginActivity.this,HomeActivity.class );
+        startActivity(RegisterIntent);
+    }
+
 
 }
