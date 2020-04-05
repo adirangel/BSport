95ff4f797272a4b77cb188cb5059b9741ae027f9
tairhadad <tairhadad1@gmail.com>
2020-04-05 16:54:33 +0300

    PM2020T7-115 sign up + Design

--- app/src/main/java/com/example/bsport/RegisterActivity.java
+++ app/src/main/java/com/example/bsport/RegisterActivity.java
@@ -115,10 +115,4 @@ public class RegisterActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
         startActivity(loginIntent);
     }
-    /*private void SendUserToMainActivity() {
-        Intent mainIntent = new Intent(RegisterActivity.this,HomeActivity.class);
-        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(mainIntent);
-        finish();
-    }*/
 }
