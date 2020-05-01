5682625ad46e8b77095185286c8ecfb8009cdfae
tairhadad <tairhadad1@gmail.com>
2020-05-01 13:18:51 +0300

    PM2020T7-144
    update add_new_activities

--- app/src/main/java/com/example/bsport/MyActivitiesFragment.java
+++ app/src/main/java/com/example/bsport/MyActivitiesFragment.java
@@ -39,46 +39,21 @@ public class MyActivitiesFragment extends Fragment {
     private static int size;
 
     private Button MyActivity;
-    private DatabaseReference RootRef;
+    private DatabaseReference RootRef,CountActivityRef;
+    static int count=0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //initilizedFields();
-
-
     }
 
-
-
-    /*private void initilizedFields() {
-        MyActivity = (Button) MyActivity.findViewById(R.id.new_activity);
-
-    }*/
-
-   /* @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Button MyActivity = (Button) View.findViewById(R.id.new_activity);
-        return inflater.inflate(R.layout.fragment_my_activities, container, false);
-    }*/
-
-   /* public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        MyActivity = (Button) view.findViewById(R.id.new_activity);
-        MyActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-    }*/
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_activities, container, false);
         Button MyActivity = (Button) view.findViewById(R.id.new_activity);
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        CountActivityRef = FirebaseDatabase.getInstance().getReference().child("Activities");
         MyActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,7 +68,6 @@ public class MyActivitiesFragment extends Fragment {
                     @Override
 
                     public void onClick(View v) {
-                        RootRef = FirebaseDatabase.getInstance().getReference();
 
                         final String name = Paper.book().read(Prevalent.UserNameKey).toString();
                         final String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());;
@@ -101,40 +75,36 @@ public class MyActivitiesFragment extends Fragment {
                         final EditText description=(EditText)mView.findViewById(R.id.activity_type);
                         final EditText number_of_players = (EditText) mView.findViewById(R.id.number_of_players);
                         final EditText game_date = (EditText) mView.findViewById(R.id.game_date);
-
-                        final String type1= type.getText().toString();
-                        final String game_date1= game_date.getText().toString();
-                        final String description1= description.getText().toString();
-                        final String number_of_players1= number_of_players.getText().toString();
                         HashMap<String,Object> userdataMap1 = new HashMap<>();
+                        CountActivityRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.exists()){
+                                    count= (int) dataSnapshot.getChildrenCount()+1;
+                                }
+                                else{
+                                    count=1;
+                                }
+                            }
 
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
                         RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                RootRef.child("Activities").addListenerForSingleValueEvent(new ValueEventListener() {
-                                    @Override
-                                    public void onDataChange(DataSnapshot dataSnapshot) {
-                                        // get total available quest
-                                         size = (int) dataSnapshot.getChildrenCount();
-                                    }
-
-                                    @Override
-                                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                    }
-                                });
-                                onDataChange(null);
-                                    int id= size+1;
                                     HashMap<String,Object> userdataMap = new HashMap<>();
                                     userdataMap.put("created_by", name);
                                     userdataMap.put("date_created", date);
-                                    userdataMap.put("description", description1);
-                                    userdataMap.put("numbers_of_players",number_of_players1);
-                                    userdataMap.put("type", type1);
-                                    userdataMap.put("game_date", game_date1);
-                                    userdataMap.put("id",id);
+                                    userdataMap.put("description", description.getText().toString());
+                                    userdataMap.put("numbers_of_players",number_of_players.getText().toString());
+                                    userdataMap.put("type", type.getText().toString());
+                                    userdataMap.put("game_date", game_date.getText().toString());
+                                    userdataMap.put("id",count);
 
-                                    RootRef.child("Activities").child(String.valueOf(id)).updateChildren(userdataMap)
+                                    RootRef.child("Activities").child(String.valueOf(count)).updateChildren(userdataMap)
                                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                                 @Override
                                                 public void onComplete(@NonNull Task<Void> task) {
@@ -147,9 +117,6 @@ public class MyActivitiesFragment extends Fragment {
                                                     }
                                                     else{
                                                         System.out.println("bad");
-
-                                                        //.dismiss();
-                                                        //Toast.makeText(RegisterActivity.this,"Network Error: Please try again after some time..", Toast.LENGTH_SHORT).show();
                                                     }
                                                 }
                                             });
