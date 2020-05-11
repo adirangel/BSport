2f1c177d9d90bea0932cc4ee4785ef3735e974cc
yonatan <yoni6232@gmail.com>
2020-05-11 16:37:37 +0300

    PM2020T7-64 PM2020T7-45 create private area

--- app/src/main/java/com/example/bsport/PrivateAreaFragment.java
+++ app/src/main/java/com/example/bsport/PrivateAreaFragment.java
@@ -1,64 +1,190 @@
 package com.example.bsport;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.bsport.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.paperdb.Paper;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PrivateAreaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PrivateAreaFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String UserAge, UserFullName, User_about_myself,UserEmail;
+    private TextView CurrentUser;
+    private DatabaseReference RootRef;
+    private View theCurrentUser;
+    private String username;
+    private TextView CurrentUserEmail,CurrentUserAge,CurrentUserDescription;
+    private Button ChangePass,ChangePersonalDetails;
 
     public PrivateAreaFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PrivateAreaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PrivateAreaFragment newInstance(String param1, String param2) {
-        PrivateAreaFragment fragment = new PrivateAreaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        username = Paper.book().read(Prevalent.UserNameKey).toString();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_private_area, container, false);
+        RootRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        theCurrentUser = inflater.inflate(R.layout.fragment_private_area, container, false);
+        ChangePass = (Button) theCurrentUser.findViewById(R.id.ChangePass);
+        ChangePersonalDetails = (Button) theCurrentUser.findViewById(R.id.ChangeDetails);
+        //TextView CurrentUserEmail;
+        //CurrentUserEmail = (TextView)theCurrentUser.findViewById(R.id.UserEmail);
+        //CurrentUserEmail.setText(Paper.book().read(Prevalent.));
+        CurrentUser = (TextView)theCurrentUser.findViewById(R.id.UserFullName);
+        CurrentUserEmail = (TextView)theCurrentUser.findViewById(R.id.UserEmail);
+        CurrentUserAge = (TextView)theCurrentUser.findViewById(R.id.UserAge);
+        CurrentUserDescription = (TextView)theCurrentUser.findViewById(R.id.User_about_myself);
+
+        RootRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    if (ds.child("username").getValue().toString().equals(username)) {
+                        UserAge = ds.child("age").getValue().toString();
+                        UserFullName = ds.child("name").getValue().toString();
+                        UserEmail = ds.child("email").getValue().toString();
+                        User_about_myself = ds.child("aboutMyself").getValue().toString();
+                    }
+                }
+
+                CurrentUser.setText(UserFullName);
+                CurrentUserAge.setText(UserAge);
+                CurrentUserEmail.setText(UserEmail);
+                CurrentUserDescription.setText(User_about_myself);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        ChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                final View mView = getLayoutInflater().inflate(R.layout.change_password, null);
+                Button newActivitySub = (Button) mView.findViewById(R.id.submit_activity1);
+                builder.setView(mView);
+                final AlertDialog dialog = builder.create();
+                dialog.show();
+                newActivitySub.setOnClickListener(new View.OnClickListener() {
+                    @Override
+
+                    public void onClick(View v) {
+                        final String password1,password2;
+                        password1 = ((EditText)mView.findViewById(R.id.change_password1)).getText().toString();
+                        password2 = ((EditText)mView.findViewById(R.id.change_password2)).getText().toString();
+                        Query Userquery = RootRef.child("Users").orderByChild(username).equalTo(username);
+                        if(password1.equals(password2)){
+
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("password", password2);
+                            RootRef.child(username).updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                        Toast.makeText(getActivity(), "הסיסמא הוחלפה בהצלחה !", Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                        }
+                        else{
+                            Toast.makeText(getActivity(), "הסיסמאות אינם שוות, נסה שנית.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                });
+            }
+        });
+        ChangePersonalDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                final View mView = getLayoutInflater().inflate(R.layout.change_personal_details, null);
+                Button newActivitySub = (Button) mView.findViewById(R.id.submit_activity2);
+                builder.setView(mView);
+                final AlertDialog dialog1 = builder.create();
+                dialog1.show();
+                newActivitySub.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String email,about,age;
+                        email = ((EditText)mView.findViewById(R.id.change_email)).getText().toString();
+                        about = ((EditText)mView.findViewById(R.id.change_about)).getText().toString();
+                        age = ((EditText)mView.findViewById(R.id.change_age)).getText().toString();
+                        Query Userquery = RootRef.child("Users").orderByChild(username).equalTo(username);
+                        if(!email.equals("")){
+
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("email", email);
+                            RootRef.child(username).updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                        Toast.makeText(getActivity(), "החלפת פרטיים אישיים בוצעה בהצלחה", Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                        }
+                        if(!about.equals("")){
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("aboutMyself",about);
+                            RootRef.child(username).updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                        Toast.makeText(getActivity(), "החלפת פרטיים אישיים בוצעה בהצלחה", Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                        }
+                        if(!age.equals("")){
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("age",age);
+                            RootRef.child(username).updateChildren(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                        Toast.makeText(getActivity(), "החלפת פרטיים אישיים בוצעה בהצלחה", Toast.LENGTH_SHORT).show();
+
+
+                                }
+                            });
+                        }
+                    }
+                });
+
+            }
+        });
+
+        return theCurrentUser;
     }
+
 }
