63d7cd929f1019bef8ea347d8be3bb13770f20a6
asafrdt <asafrdt@gmail.com>
2020-05-13 16:46:43 +0300

    PM2020T7-127 PM2020T7-128 PM2020T7-101 PM2020T7-103 Create list of activities UI import from database of activities

--- app/src/main/java/com/example/bsport/ActivitiesListFragment.java
+++ app/src/main/java/com/example/bsport/ActivitiesListFragment.java
@@ -1,30 +1,142 @@
 package com.example.bsport;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.FirebaseAppLifecycleListener;
+import com.example.bsport.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+import io.paperdb.Paper;
 
 
 public class ActivitiesListFragment extends Fragment {
 
+    private Button MyActivity;
+    private DatabaseReference RootRef,CountActivityRef;
+    private static int count=0;
+    private RecyclerView recyclerView;
+    private View view;
+    private ListOfActivitiesAdapter list_of_activity_adapter;
+    private String username = Paper.book().read(Prevalent.UserNameKey).toString();
+    private ArrayList<String> My_Created_by = new ArrayList<>();
+    private ArrayList<String> My_name_activity = new ArrayList<>();
+    private ArrayList<String> My_activity_type = new ArrayList<>();
+    private ArrayList<String> My_game_date = new ArrayList<>();
+    private ArrayList<String> My_location = new ArrayList<>();
 
-    public ActivitiesListFragment() {
-        // Required empty public constructor
-    }
+    private ArrayList<String> locations = new ArrayList<>();
 
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activities_list, container, false);
+        view = inflater.inflate(R.layout.fragment_activities_list, container, false);
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        CountActivityRef = FirebaseDatabase.getInstance().getReference().child("Activities");
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewNewActivity);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        try {
+            // get JSONObject from JSON file
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            // fetch JSONArray named users
+            JSONArray facArray = obj.getJSONArray("Facilities");
+            // implement for loop for getting users list data
+            for (int i = 0; i < facArray.length(); i++) {
+
+                // create a JSONObject for fetching single user data
+                JSONObject facDetail = facArray.getJSONObject(i);
+                // fetch email and name and store it in arraylist
+                if(!(locations.contains("רחוב: " + facDetail.getString("street"))) && !facDetail.getString("street").equals(""))
+                    locations.add("רחוב: " + facDetail.getString("street"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+
+        CountActivityRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                My_name_activity.clear();
+                My_activity_type.clear();
+                My_game_date.clear();
+                My_location.clear();
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                        My_name_activity.add("שם הפעילות: "+ds.child("description").getValue().toString());
+                        My_activity_type.add("סוג הפעילות: "+ds.child("type").getValue().toString());
+                        My_game_date.add("תאריך הפעילות: "+ ds.child("game_date").getValue().toString());
+                        My_location.add("מיקום הפעילות: "+ds.child("location").getValue().toString());
+
+                }
+                list_of_activity_adapter = new ListOfActivitiesAdapter(My_name_activity,My_activity_type,My_game_date,My_location);
+                recyclerView.setAdapter(list_of_activity_adapter); // set the Adapter to RecyclerView
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return view;
     }
 
+    private String loadJSONFromAsset() {
+        String Context = null;
+        String json = null;
+        try {
+            InputStream is = getContext().getAssets().open("dataBaseSport.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
 }
---
+++ app/src/main/java/com/example/bsport/ListOfActivitiesAdapter.java
@@ -0,0 +1,75 @@
+package com.example.bsport;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bsport.Prevalent.Prevalent;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import io.paperdb.Paper;
+
+public class ListOfActivitiesAdapter extends RecyclerView.Adapter<ListOfActivitiesAdapter.MyViewHolder> {
+
+    ArrayList<String> My_name_activity;
+    ArrayList<String> My_activity_type;
+    ArrayList<String> My_game_date;
+    ArrayList<String> My_location;
+
+
+    public ListOfActivitiesAdapter(ArrayList<String> My_name_activity,ArrayList<String> My_activity_type,ArrayList<String> My_game_date,ArrayList<String> My_location) {
+        this.My_name_activity=My_name_activity;
+        this.My_activity_type=My_activity_type;
+        this.My_game_date=My_game_date;
+        this.My_location=My_location;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_of_activities, parent, false);
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        holder.name.setText(My_name_activity.get(position));
+        holder.type.setText(My_activity_type.get(position));
+        holder.game_date.setText(My_game_date.get(position));
+        holder.location.setText(My_location.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return My_activity_type.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView type, name, game_date, location;// init the item view's
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.name_activity);
+            type = (TextView) itemView.findViewById(R.id.activity_type1);
+            game_date = (TextView) itemView.findViewById(R.id.game_date1);
+            location = (TextView) itemView.findViewById(R.id.location);
+
+
+        }
+    }
+}
