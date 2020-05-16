6f92a3d79394d96496da4400f67b752ae6ce0d72
asafrdt <asafrdt@gmail.com>
2020-05-16 11:42:43 +0300

    fix bugs

--- app/src/test/java/com/example/bsport/CustomAdapterTest.java
+++ app/src/test/java/com/example/bsport/CustomAdapterTest.java
@@ -5,75 +5,75 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class SportFacFragmentTest {
+public class CustomAdapterTest {
     @Test
     public void descriptionEmpty_oneTest_ReturnTrue() {
-        assertTrue(SportFacFragment.Checking_description_Empty(""));
+        assertTrue(CustomAdapter.Checking_description_Empty(""));
     }
 
     @Test
     public void descriptionEmpty_oneTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_description_Empty("1. משחק כדורסל"));
+        assertFalse(CustomAdapter.Checking_description_Empty("1. משחק כדורסל"));
     }
 
     @Test
     public void descriptionEmpty_twoTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_description_Empty("football 5X5"));
+        assertFalse(CustomAdapter.Checking_description_Empty("football 5X5"));
     }
 
     @Test
     public void Checking_number_of_players_Empty_oneTest_ReturnTrue() {
-        assertTrue(SportFacFragment.Checking_number_of_players_Empty(""));
+        assertTrue(CustomAdapter.Checking_number_of_players_Empty(""));
     }
 
     @Test
     public void Checking_number_of_players_Empty_oneTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_description_Empty("football 5X5"));
+        assertFalse(CustomAdapter.Checking_description_Empty("football 5X5"));
     }
 
     @Test
     public void Checking_number_of_players_Empty_twoTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_description_Empty("4"));
+        assertFalse(CustomAdapter.Checking_description_Empty("4"));
     }
 
     @Test
     public void Checking_number_of_players_valid_value_oneTest_ReturnTrue() {
-        assertTrue(SportFacFragment.Checking_number_of_players_valid_value("football"));
+        assertTrue(CustomAdapter.Checking_number_of_players_valid_value("football"));
     }
     @Test
     public void Checking_number_of_players_valid_value_oneTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_number_of_players_valid_value("5"));
+        assertFalse(CustomAdapter.Checking_number_of_players_valid_value("5"));
     }
     @Test
     public void Checking_game_date_Empty_oneTest_ReturnTrue() {
-        assertTrue(SportFacFragment.Checking_game_date_Empty(""));
+        assertTrue(CustomAdapter.Checking_game_date_Empty(""));
     }
     @Test
     public void Checking_game_date_Empty_oneTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_game_date_Empty("5"));
+        assertFalse(CustomAdapter.Checking_game_date_Empty("5"));
     }
     @Test
     public void Checking_game_date_Empty_twoTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_game_date_Empty("30-02-2020"));
+        assertFalse(CustomAdapter.Checking_game_date_Empty("30-02-2020"));
     }
     @Test
     public void Checking_game_date_valid_value_oneTest_ReturnTrue() {
-        assertTrue(SportFacFragment.Checking_game_date_valid_value(""));
+        assertTrue(CustomAdapter.Checking_game_date_valid_value(""));
     }
     @Test
     public void Checking_game_date_valid_value_twoTest_ReturnTrue() {
-        assertTrue(SportFacFragment.Checking_game_date_valid_value("30/02/2020"));
+        assertTrue(CustomAdapter.Checking_game_date_valid_value("30/02/2020"));
     }
     @Test
     public void Checking_game_date_valid_value_oneTest_assertFalse() {
-        assertFalse(SportFacFragment.Checking_game_date_valid_value("30-02-2020"));
+        assertFalse(CustomAdapter.Checking_game_date_valid_value("30-02-2020"));
     }
     @Test
     public void Check_that_date_after_today_oneTest_ReturnTrue() {
-        assertTrue(SportFacFragment.Check_that_date_after_today("03-05-2020","04-05-2020"));
+        assertTrue(CustomAdapter.Check_that_date_after_today("03-05-2020","04-05-2020"));
     }
     @Test
     public void Check_that_date_after_today_oneTest_assertFalse() {
-        assertFalse(SportFacFragment.Check_that_date_after_today("03-05-2020","02-05-2020"));
+        assertFalse(CustomAdapter.Check_that_date_after_today("03-05-2020","02-05-2020"));
     }
 }
