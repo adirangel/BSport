846cfbee8ed35a1570c11066808b50580e12dcb4
Adir <adirangel2@gmail.com>
2020-06-10 13:54:10 +0300

    Checkstyle Measurement error updates

--- app/src/main/java/com/example/bsport/ActivitiesListFragment.java
+++ app/src/main/java/com/example/bsport/ActivitiesListFragment.java
@@ -1,13 +1,8 @@
 package com.example.bsport;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,11 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
