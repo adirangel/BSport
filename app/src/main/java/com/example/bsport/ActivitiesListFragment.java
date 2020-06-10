efc14c6ee05644f2d354c7f8bcbfe1180b2e0900
Adir <adirangel2@gmail.com>
2020-06-10 18:59:09 +0300

    Checkstyle Measurement error updates

--- app/src/main/java/com/example/bsport/ActivitiesListFragment.java
+++ app/src/main/java/com/example/bsport/ActivitiesListFragment.java
@@ -11,18 +11,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.bsport.Prevalent.Prevalent;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONArray;
@@ -31,15 +24,8 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
 
-import io.paperdb.Paper;
 
 
 public class ActivitiesListFragment extends Fragment {
